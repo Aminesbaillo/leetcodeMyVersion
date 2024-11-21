fbd6e51d9f080d56b41c615507fa3eb92dcb6995
Aminesbaillo <sbailloamine42@gmail.com>
2024-11-21 10:22:20 +0100

    resolving 586. Customer Placing the Largest Number of Ordersé

---
+++ 586. Customer Placing the Largest Number of Orders.py
@@ -0,0 +1,27 @@
+import pandas as pd 
+data = {
+    'order_number': [1, 2, 3, 4, 5],
+    'customer_number': [1, 1, 2, 3, 3]
+}
+data = pd.DataFrame(data = data) 
+print('dataframe :') 
+print(data)
+print('------------------------------------------------------------------------------------------------')
+print('------------------------------------------------------------------------------------------------')
+print('------------------------------------------------------------------------------------------------')
+
+
+def largest_orders(orders: pd.DataFrame) -> pd.DataFrame:
+    results = orders.groupby('customer_number')['order_number'].count().reset_index()
+    print('results  :')
+    print(results)
+    print('------------------------------------------------------------------------------------------------')
+
+    max_order = results[results.order_number == results.order_number.max()]
+    print('max_order: ')
+    print(max_order)
+    print('------------------------------------------------------------------------------------------------')
+    print('results finale :')
+    return max_order[['customer_number']]
+
+print(largest_orders(data))
